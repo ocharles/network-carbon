92d13c5e3c1e1b3c8a51da0093add5709b77baf7
Oliver Charles <ollie@ocharles.org.uk>
2015-01-16 19:48:04 +0000

    Regenerate nix expressions

--- default.nix
+++ default.nix
@@ -1,14 +1,12 @@
-{ cabal, network, text, time, vector }:
-
-cabal.mkDerivation (self: {
+{ mkDerivation, base, bytestring, network, stdenv, text, time
+, vector
+}:
+mkDerivation {
   pname = "network-carbon";
-  version = "1.0.1";
+  version = "1.0.2";
   src = ./.;
-  buildDepends = [ network text time vector ];
-  meta = {
-    homepage = "http://github.com/ocharles/network-carbon";
-    description = "A Haskell implementation of the Carbon protocol (part of the Graphite monitoring tools)";
-    license = self.stdenv.lib.licenses.bsd3;
-    platforms = self.ghc.meta.platforms;
-  };
-})
+  buildDepends = [ base bytestring network text time vector ];
+  homepage = "http://github.com/ocharles/network-carbon";
+  description = "A Haskell implementation of the Carbon protocol (part of the Graphite monitoring tools)";
+  license = stdenv.lib.licenses.bsd3;
+}
